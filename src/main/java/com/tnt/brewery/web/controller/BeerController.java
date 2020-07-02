e5dca03fed0c0a76e7245f03e81b01cb75a43862
somnath46 <4684.somnath@gmail.com>
2020-07-02 11:07:34 +0530

    assignment-2: post method stub

--- src/main/java/com/tnt/brewery/web/controller/BeerController.java
+++ src/main/java/com/tnt/brewery/web/controller/BeerController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,11 @@ public class BeerController {
 	@GetMapping("/{beerId}")
 	public ResponseEntity<BeerDto> getBeer(@PathVariable final UUID beerId) {
 		return new ResponseEntity<BeerDto>(beerService.getBeerById(beerId), HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity createBear(@RequestBody BeerDto beerDto) {
+		// TODO: create beer
+		return null;
 	}
 }
